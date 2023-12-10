cfe9ef2218631a81cbaae91cb93322f6285d2446
amitagg3 <amitagg3@gmail.com>
2023-12-10 16:57:02 +0530

    Leetcode-1160 Find Words That Can Be Formed by Characters

---
+++ src/main/java/com/amit/javaPractise/CountWordsFormedByCharacter.java
@@ -0,0 +1,53 @@
+package com.amit.javaPractise;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/*
+1160. Find Words That Can Be Formed by Characters
+
+You are given an array of strings words and a string chars.
+A string is good if it can be formed by characters from chars (each character can only be used once).
+Return the sum of lengths of all good strings in words.
+
+Example 1:
+
+Input: words = ["cat","bt","hat","tree"], chars = "atach"
+Output: 6
+Explanation: The strings that can be formed are "cat" and "hat" so the answer is 3 + 3 = 6.
+ */
+
+public class CountWordsFormedByCharacter {
+
+    public static void main(String[] args) {
+//        String[] words = {"caat","bt","hat","tree"};
+//        String chars = "atach";
+        String[] words = {"dyiclysmffuhibgfvapygkorkqllqlvokosagyelotobicwcmebnpznjbirzrzsrtzjxhsfpiwyfhzyonmuabtlwin", "ndqeyhhcquplmznwslewjzuyfgklssvkqxmqjpwhrshycmvrb", "ulrrbpspyudncdlbkxkrqpivfftrggemkpyjl", "boygirdlggnh", "xmqohbyqwagkjzpyawsydmdaattthmuvjbzwpyopyafphx", "nulvimegcsiwvhwuiyednoxpugfeimnnyeoczuzxgxbqjvegcxeqnjbwnbvowastqhojepisusvsidhqmszbrnynkyop", "hiefuovybkpgzygprmndrkyspoiyapdwkxebgsmodhzpx", "juldqdzeskpffaoqcyyxiqqowsalqumddcufhouhrskozhlmobiwzxnhdkidr", "lnnvsdcrvzfmrvurucrzlfyigcycffpiuoo", "oxgaskztzroxuntiwlfyufddl", "tfspedteabxatkaypitjfkhkkigdwdkctqbczcugripkgcyfezpuklfqfcsccboarbfbjfrkxp", "qnagrpfzlyrouolqquytwnwnsqnmuzphne", "eeilfdaookieawrrbvtnqfzcricvhpiv", "sisvsjzyrbdsjcwwygdnxcjhzhsxhpceqz", "yhouqhjevqxtecomahbwoptzlkyvjexhzcbccusbjjdgcfzlkoqwiwue", "hwxxighzvceaplsycajkhynkhzkwkouszwaiuzqcleyflqrxgjsvlegvupzqijbornbfwpefhxekgpuvgiyeudhncv", "cpwcjwgbcquirnsazumgjjcltitmeyfaudbnbqhflvecjsupjmgwfbjo", "teyygdmmyadppuopvqdodaczob", "qaeowuwqsqffvibrtxnjnzvzuuonrkwpysyxvkijemmpdmtnqxwekbpfzs", "qqxpxpmemkldghbmbyxpkwgkaykaerhmwwjonrhcsubchs"};
+        String chars = "usdruypficfbpfbivlrhutcgvyjenlxzeovdyjtgvvfdjzcmikjraspdfp";
+        System.out.println(countCharacters(words, chars));
+
+    }
+
+    public static int countCharacters(String[] words, String chars) {
+        int count = 0;
+        Map<Character, Integer> charCount = new HashMap<>();
+        for (Character c : chars.toCharArray()) {
+            charCount.put(c, charCount.getOrDefault(c, 0) + 1);
+        }
+        for (String word : words) {
+            Map<Character, Integer> specificWordCharCount = new HashMap<>();
+            boolean canForm = true;
+            for (Character c : word.toCharArray()) {
+                if (!charCount.containsKey(c) || Objects.equals(charCount.get(c), specificWordCharCount.get(c))) {
+                    canForm = false;
+                    break;
+                }
+                specificWordCharCount.put(c, specificWordCharCount.getOrDefault(c, 0) + 1);
+            }
+            if (canForm)
+                count += word.length();
+        }
+        return count;
+    }
+}
