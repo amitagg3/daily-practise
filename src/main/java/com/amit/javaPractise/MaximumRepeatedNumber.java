dd8f59dcb2a3cda0648486f5c892dc697c0ab60b
amitagg3-vlink <amit.agarwal@vlinkinfo.com>
2023-12-11 20:52:07 +0530

    Maximum Repeating number by one for loop

---
+++ src/main/java/com/amit/javaPractise/MaximumRepeatedNumber.java
@@ -0,0 +1,29 @@
+package com.amit.javaPractise;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/*
+Find maximum repeated number in an array using one for loop
+
+ */
+public class MaximumRepeatedNumber {
+    public static void main(String[] args) {
+        int[] a = {1, 1, 2, 1, 5, 6, 6, 6, 6, 6, 8, 5, 9, 1, 7, 1};
+        int max = 0;
+        int maxNum = 0;
+        int maxTemp;
+        //To convert int[] array to Integer List
+        List<Integer> list = Arrays.stream(a).boxed().collect(Collectors.toList());
+        for (Integer i : list) {
+            maxTemp = Collections.frequency(list, i);
+            if (max <= maxTemp) {
+                max = maxTemp;
+                maxNum = i;
+            }
+        }
+        System.out.println(maxNum);
+
+
+    }
+}
