b9a3658aba2d6a9aa46e457e120106aa05be2f9d
Amit Aggarwal <amitagg3@gmail.com>
2023-12-24 23:59:04 +0530

    Leetcode-80 : Remove Duplicates from Sorted Array II

---
+++ src/main/java/com/amit/javaPractise/RemoveDuplicateFromSortedArrayII.java
@@ -0,0 +1,12 @@
+package com.amit.javaPractise;
+
+public class RemoveDuplicateFromSortedArrayII {
+    public static void main(String[] args) {
+        int[] nums = {1,1,1,2,2,3};
+        System.out.println(removeDuplicates(nums));
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        return 1;
+    }
+}
