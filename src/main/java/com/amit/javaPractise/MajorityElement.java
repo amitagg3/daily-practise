cc8b30f0692eaf06b15d8ea6307702af8062c627
Amit Aggarwal <amitagg3@gmail.com>
2023-12-25 23:44:52 +0530

    Leetcode-169 : Majority Element

---
+++ src/main/java/com/amit/javaPractise/MajorityElement.java
@@ -0,0 +1,35 @@
+package com.amit.javaPractise;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+Leetcode-169 : Majority Element
+
+Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+Example 1:
+Input: nums = [3,2,3]
+Output: 3
+
+Example 2:
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+ */
+public class MajorityElement {
+    public static void main(String[] args) {
+        int[] nums = {3, 2, 3};
+        System.out.println(majorityElement(nums));
+    }
+
+    public static int majorityElement(int[] nums) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for (int num : nums) {
+            hm.put(num, hm.getOrDefault(num, 0) + 1);
+        }
+        return Collections.max(hm.entrySet(), Map.Entry.comparingByValue()).getKey();
+    }
+}
