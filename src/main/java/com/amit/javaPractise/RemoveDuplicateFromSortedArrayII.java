c1c864005960cff5367b7f54d26700fe0f5e7b0a
Amit Aggarwal <amitagg3@gmail.com>
2024-01-01 21:22:34 +0530

    Leetcode-80 : Remove Duplicates from Sorted Array II

--- src/main/java/com/amit/javaPractise/RemoveDuplicateFromSortedArrayII.java
+++ src/main/java/com/amit/javaPractise/RemoveDuplicateFromSortedArrayII.java
@@ -1,45 +1,41 @@
 package com.amit.javaPractise;
+/*
+Leetcode-80 : Remove Duplicates from Sorted Array II
 
-import java.util.Arrays;
+Given an integer array nums sorted in non-decreasing order, remove some duplicates in-place such that each unique element appears at most twice. The relative order of the elements should be kept the same.
 
+Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
+
+Return k after placing the final result in the first k slots of nums.
+
+Example 1:
+Input: nums = [1,1,1,2,2,3]
+Output: 5, nums = [1,1,2,2,3,_]
+Explanation: Your function should return k = 5, with the first five elements of nums being 1, 1, 2, 2 and 3 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+
+Example 2:
+Input: nums = [0,0,1,1,1,1,2,3,3]
+Output: 7, nums = [0,0,1,1,2,3,3,_,_]
+Explanation: Your function should return k = 7, with the first seven elements of nums being 0, 0, 1, 1, 2, 3 and 3 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+ */
 public class RemoveDuplicateFromSortedArrayII {
     public static void main(String[] args) {
-        int[] nums = {0,0,1,1,1,1,2,3,3};
+        int[] nums = {0, 0, 1, 1, 1, 1, 2, 3, 3};
         System.out.println(removeDuplicates(nums));
     }
 
     public static int removeDuplicates(int[] nums) {
-        int sub=0;
-        int count=1;
-        for(int i=1;i<nums.length;i++){
-            if(nums[i]==nums[i-1]){
-                count++;
-            }else{
-                count=1;
+        int len = nums.length;
+        if (len <= 2) return len;
+        int pointer = 2;
+        for (int i = 2; i < len; i++) {
+            if (nums[i] != nums[pointer - 2]) {
+                nums[pointer] = nums[i];
+                pointer++;
             }
-            if(count>2){
-                int j=i+1;
-                int k=i;
-                while(nums[i]<nums[j]) {
-                    swapArray(nums, k, j);
-                    count = 0;
-                    k++;
-                    j++;
-                }
-                sub++;
-            }
-
-
         }
-        System.out.println(Arrays.toString(nums));
-
-        return nums.length-sub;
-    }
-
-    public static void swapArray(int[] nums,int start,int end){
-        while(start<end-1){
-            nums[start]=nums[++start];
-        }
-
+        return pointer;
     }
 }
